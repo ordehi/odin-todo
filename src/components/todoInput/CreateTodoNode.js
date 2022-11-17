6ab3747131f9586e4e6a6977d7c398d4b98fa97b
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-11-17 07:51:24 -0400

    add comment about fixing priority and dueDate

--- src/components/todoInput/CreateTodoNode.js
+++ src/components/todoInput/CreateTodoNode.js
@@ -5,6 +5,7 @@ import "../../styles/createTodo.css";
 
 const CreateTodoNode = ({ updateHandler }) => {
   const attrs = { class: "create-todo" };
+  // TODO: need to fix setting priority and dueDate after modal implementation
   const todoChange = TodoChange({});
   const addTodoBtn = AddTodoBtn({ updateHandler });
   const children = [todoChange, addTodoBtn];
